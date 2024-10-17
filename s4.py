eb6771b6df1fcde1b97fba8a10f8ca27902ff3c7
AMMARELKURDI <155187194+AMMARELKURDI@users.noreply.github.com>
2024-10-17 22:05:43 +0300

    Update s4.py

--- s4.py
+++ s4.py
@@ -3,8 +3,8 @@ import numpy as np
 import pandas as pd
 #import matplotlib.pyplot as plt
 #import seaborn as sns
-import plotly.express as px
 import plotly.graph_objects as go
+import plotly.express as px
 
 st.set_page_config(page_title="Tips Dashboard",layout="wide",initial_sidebar_state="auto")
 
